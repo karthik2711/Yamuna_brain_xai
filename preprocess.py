5065557de78f94a040026d16096c22d26157ad82
karthik2711 <bvkarthik2711@gmail.com>
2026-01-16 19:19:31 +0530

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,14 +1,17 @@
+import cv2
 import numpy as np
-import pandas as pd
-from sklearn.preprocessing import StandardScaler
+import os
 
-def preprocess_data(file_path):
-    data = pd.read_csv(file_path)
-    
-    X = data.iloc[:, :-1].values
-    y = data.iloc[:, -1].values
-    
-    scaler = StandardScaler()
-    X_scaled = scaler.fit_transform(X)
-    
-    return X_scaled, y
+def load_images(folder, img_size=(224,224)):
+    images = []
+    labels = []
+    for label, cls in enumerate(os.listdir(folder)):
+        cls_path = os.path.join(folder, cls)
+        for img in os.listdir(cls_path):
+            img_path = os.path.join(cls_path, img)
+            im = cv2.imread(img_path)
+            im = cv2.resize(im, img_size)
+            im = im / 255.0
+            images.append(im)
+            labels.append(label)
+    return np.array(images), np.array(labels)
